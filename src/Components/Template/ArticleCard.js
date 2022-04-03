72ca40e39f5317a16fdd80a557181fcd80cfa35f
chh <3492243557@qq.com>
2022-04-03 10:22:37 +0800

    手机适配-字体和卡片背景

--- src/Components/Template/ArticleCard.js
+++ src/Components/Template/ArticleCard.js
@@ -7,19 +7,39 @@ const ArticleCard = (props) => {
     let picid = 'https://cdn.jsdelivr.net/gh/chh0/pics/' + props.mdnum + '.jpg'
     let digest = all.articles[mdid].con.split("<p>")[1]
     digest = digest.split("</p>")[0]
-    let ImgStyle = {
-        "float": "left",
-        "width": "30%",
-        "height": "200px",
-        "background": `url(${picid}) no-repeat center center`,
-        "background-size": "cover",
-        "border-top-left-radius": "50px",
-        "border-bottom-left-radius": "50px",
+    let ImgStyle = {}
+    let CardStyle = {}
+    let ConStyle = {}
+    if(document.body.clientWidth > 901){
+        ImgStyle = {
+            "float": "left",
+            "width": "30%",
+            "height": "200px",
+            "background": `url(${picid}) no-repeat center center`,
+            "background-size": "cover",
+            "border-top-left-radius": "50px",
+            "border-bottom-left-radius": "50px",
+        }
+    }else{
+        ImgStyle = {
+            "position": "fixed",
+            "width": "0"
+        }
+        CardStyle = {
+            "background": `url(${picid}) no-repeat center center`,
+            "background-size": "cover",
+        }
+        ConStyle = {
+            "width": "100%",
+            "background-color": "rgba(255, 255, 255, 0.4)",
+            "border-radius": "50px",
+        }
     }
+    
     return (
-        <div className="ArticleCard">
+        <div className="ArticleCard" style={CardStyle}>
             <div className="ArticleCardImg" style={ImgStyle}></div>
-            <div className="ArticleCardCon">
+            <div className="ArticleCardCon" style={ConStyle}>
                 <h1 className="ArticleCardTitle">{all.articles[mdid].title}</h1>
                 <p className="ArticleCardText">{digest}</p>
             </div>
