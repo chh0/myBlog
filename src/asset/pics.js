47ffe013e88997a4e945a7c92ffce5e9d4910723
chh <3492243557@qq.com>
2022-09-11 21:22:32 +0800

    路过图床不给穿二次元，背景图改到了smms.app上

--- src/asset/pics.js
+++ src/asset/pics.js
@@ -1,19 +1,18 @@
 const pics = {
-    "bg.jpg":"https://s1.ax1x.com/2022/09/11/vOcM5R.jpg",
-    "1.jpg":"https://s1.ax1x.com/2022/09/11/vOcEvT.jpg",
-    "2.jpg":"https://s1.ax1x.com/2022/09/11/vOcmb4.jpg",
-    "3.jpg":"https://s1.ax1x.com/2022/09/11/vOcuVJ.jpg",
-    "4.jpg":"https://s1.ax1x.com/2022/09/11/vOc18x.jpg",
-    "5.jpg":"https://s1.ax1x.com/2022/09/11/vOc8xK.jpg",
-    "6.jpg":"https://s1.ax1x.com/2022/09/11/vOc326.jpg",
-    "7.jpg":"https://s1.ax1x.com/2022/09/11/vOcYrD.jpg",
-    "8.jpg":"https://s1.ax1x.com/2022/09/11/vOcJKO.jpg",
-    "9.jpg":"https://s1.ax1x.com/2022/09/11/vOcUVH.jpg",
-    "10.jpg":"https://s1.ax1x.com/2022/09/11/vOcdIA.jpg",
-    "11.jpg":"https://s1.ax1x.com/2022/09/11/vOctqe.jpg",
-    "12.jpg":"https://s1.ax1x.com/2022/09/11/vOcaad.jpg",
-    "13.jpg":"https://s1.ax1x.com/2022/09/11/vOcKa9.jpg",
-    "14.jpg":"https://s1.ax1x.com/2022/09/11/vOclP1.jpg"
+    "bg.jpg":"https://s2.loli.net/2022/09/11/YLc3GRNxnM9Q684.jpg",
+    "1.jpg":"https://s2.loli.net/2022/09/11/BHSYni13v2E48Db.jpg",
+    "2.jpg":"https://s2.loli.net/2022/09/11/GyXZuzCi1qr3E4d.jpg",
+    "3.jpg":"https://s2.loli.net/2022/09/11/QrJbEhIPp1mFSn6.jpg",
+    "4.jpg":"https://s2.loli.net/2022/09/11/M4TgdpR5zrhSDH9.jpg",
+    "5.jpg":"https://s2.loli.net/2022/09/11/Z1K5jxPpJSHtMlv.jpg",
+    "6.jpg":"https://s2.loli.net/2022/09/11/eMlzrTR5gWmPsOd.jpg",
+    "7.jpg":"https://s2.loli.net/2022/09/11/q1jCWByrQ78ocUx.jpg",
+    "8.jpg":"https://s2.loli.net/2022/09/11/PtpjOx2qIeXNncv.jpg",
+    "9.jpg":"https://s2.loli.net/2022/09/11/7vRcJ1wmZYnalhI.jpg",
+    "10.jpg":"https://s2.loli.net/2022/09/11/RaiClAcMLN3kBo6.jpg",
+    "11.jpg":"https://s2.loli.net/2022/09/11/1yFRo6WhmY9L3B8.jpg",
+    "12.jpg":"https://s2.loli.net/2022/09/11/LMtR7Tc3HifmlI9.jpg",
+    "13.jpg":"https://s2.loli.net/2022/09/11/j5HCv93EKJMyXtR.jpg",
 }
 
 export default pics
